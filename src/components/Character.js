3ef7a16c1ce9e0c58d767c3538bc207a2cc5ec70
Kenan Dolic <kdolic@alumni.iastate.edu>
2020-12-11 11:05:10 -0600

    Character Component/Rendered on App

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+
+import React from 'react'
+
+export default function Character(props) {
+
+    const {characters, character} = props
+    return (
+        <div>
+            {characters.map(character => {
+                return(
+                    <div>
+                        <img src={character.image} alt={character.name} />
+                        <h2>Name: {character.name}</h2>
+                        <h3>Status: {character.status}</h3>
+                        <h3>Species: {character.species}</h3>
+                        <h3>Gender: {character.gender}</h3>
+                        <h3>Origin: {character.origin.name}</h3>
+                    </div>
+                );
+            })}
+            
+        </div>
+    )
+}
